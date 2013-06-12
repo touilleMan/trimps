4d94912881ecea38c6193754093e5753e06d7f44
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2013-06-12 22:02:24 +0200

    Improving the CPU, almost done...

--- cpu.py
+++ cpu.py
@@ -2,143 +2,169 @@ import struct
 
 class LoaderError(Exception):
     def __init__(self, value):
+        super(LoaderError, self).__init__()
         self.value = value
     def __str__(self):
         return repr(self.value)
 
 
 class Program():
-	"""Handle de input MIPS binary"""
-	instructions = []
+    """Handle de input MIPS binary"""
+    instructions = []
 
-	def load(self, path):
-		# Get the input binary as bit array
-		with open(path, "rb") as fd:
-			data = fd.read()
-			if len(data) % 4 != 0:
-				raise LoaderError('"{}" must be a multiple of 4 bytes'
-					'(size : {} byts'.format(path, len(data)))
-			for i in xrange(len(data) / 4):
-				unpacked = struct.unpack(">i", data[i * 4 : i * 4 + 4])
-				self.instructions.append(unpacked[0])
+    def load(self, path):
+        # Get the input binary as bit array
+        with open(path, "rb") as fd:
+            data = fd.read()
+            if len(data) % 4 != 0:
+                raise LoaderError('"{}" must be a multiple of 4 bytes'
+                    '(size : {} byts'.format(path, len(data)))
+            for i in xrange(len(data) / 4):
+                unpacked = struct.unpack(">i", data[i * 4 : i * 4 + 4])
+                self.instructions.append(unpacked[0])
 
-	def fetch(self, pc):
-		"""Return the requested instruction if it exists or 0x00000000"""
-		print "Fetch : " + bin(self.instructions[pc])
-		if len(self.instructions) > (pc / 4):
-			return self.instructions[pc]
-		else:
-			return 0x00000000
+    def fetch(self, pc):
+        """Return the requested instruction if it exists or 0x00000000"""
+        if len(self.instructions) > (pc / 4):
+            return self.instructions[pc / 4]
+        else:
+            return 0x00000000
 
+
+class Memory():
+    """RAM and I/O are stored here"""
+
+    def __init__(self):
+        self.mem = {}
+
+    def __getitem__(self, address):
+        # Check if the address point on something
+        if self.mem[address] is None:
+            return 0x0
+        return self.mem[address]
+
+    def __setitem__(self, address, item):
+        self.mem[address] = item
 
 class Cpu():
-	"""Emulate the embedded MIPS CPU"""
+    """Emulate the embedded MIPS CPU"""
 
-	def __init__(self):
-		self.pc = 0
-		# MIPS has 31 general-purpose registers plus r0 (always 0 register)
-		self.r = [ 0x00000000 for i in xrange(32) ]
-		self.program = Program()
+    def __init__(self):
+        self.pc = 0
+        # MIPS has 31 general-purpose registers plus r0 (always 0 register)
+        self.r = [ 0x00000000 for _ in xrange(32) ]
+        self.program = Program()
+        self.memory = Memory()
 
-	def step(self):
-		"""Run the CPU to compute the next operation"""
-		instruction = self.program.fetch(self.pc)
-		self.execute(instruction)
-		# r0 is always == 0x0, reset it in case it has changed
-		self.r[0] == 0x00000000
-		self.pc += 4
+    def step(self):
+        """Run the CPU to compute the next operation"""
+        instruction = self.program.fetch(self.pc)
+        print "pc : " + str(self.pc),
+        self.execute(instruction)
+        # r0 is always == 0x0, reset it in case it has changed
+        self.r[0] = 0x00000000
+        self.pc += 4
 
-	def execute(self, instruction):
-		"""Make the CPU run the given instruction"""
+    def execute(self, instruction):
+        """Make the CPU run the given instruction"""
 
-		OPCODES_R = {
-		0 : self.execute_R
-		}
+        OPCODES_R = {
+        0 : self.execute_R
+        }
 
-		OPCODES_I = {
-		0x04 : self.execute_I_BEQ,
-		0x23 : self.execute_I_LW,
-		0x2b : self.execute_I_SW,
-		0x0c : self.execute_I_ANDI,
-		0x0d : self.execute_I_ORI,
-		0x08 : self.execute_I_ADDI
-		}
+        OPCODES_I = {
+        0x04 : [ self.execute_I_BEQ, "BEQ" ],
+        0x23 : [ self.execute_I_LW, "LW" ],
+        0x2b : [ self.execute_I_SW, "SW" ],
+        0x0c : [ self.execute_I_ANDI, "ANDI" ],
+        0x0d : [ self.execute_I_ORI, "ORI" ],
+        0x08 : [ self.execute_I_ADDI, "ADDI" ]
+        }
 
-		OPCODES_J = {
-		0x02 : self.execute_J_JUMP
-		}
+        OPCODES_J = {
+        0x02 : self.execute_J_JUMP
+        }
 
-		# Get the instruction type (R, I or J) from the opcode and execute it
-		opcode = instruction & 0xFC000000
-		if opcode in OPCODES_R:
-			rs = instruction & 0x03E00000
-			rt = instruction & 0x001F0000
-			rd = instruction & 0x0000F800
-			shamt = instruction & 0x000007C0
-			funct = instruction & 0x0000003F
-			OPCODES_R[opcode](rs, rt, rd, shamt, funct)
+        # Get the instruction type (R, I or J) from the opcode and execute it
+        opcode = (instruction >> 26) & 0x3F
+        if opcode in OPCODES_R:
+            rs = (instruction >> 21) & 0x1F
+            rt = (instruction >> 16) & 0x1F
+            rd = (instruction >> 11) & 0x1F
+            shamt = (instruction >> 6) & 0x1F
+            funct = instruction & 0x3F
+            print "R funct : " + hex(funct)
+            OPCODES_R[opcode](rs, rt, rd, shamt, funct)
 
-		elif opcode in OPCODES_I:
-			print OPCODES_I[opcode]
-			rs = instruction & 0x03E00000
-			rt = instruction & 0x001F0000
-			immed = instruction & 0x0000FFFF
-			OPCODES_I[opcode](rs, rt, immed)
+        elif opcode in OPCODES_I:
+            rs = (instruction >> 21) & 0x1F
+            rt = (instruction >> 16) & 0x1F
+            immed = instruction & 0xFFFF
+            print "I immed : " + hex(immed) + " " + OPCODES_I[opcode][1]
+            OPCODES_I[opcode][0](rs, rt, immed)
 
-		elif opcode in OPCODES_J:
-			print OPCODES_J[opcode]
-			addr = instruction & 0x03FFFFFF
-			OPCODES_J[opcode](addr)
+        elif opcode in OPCODES_J:
+            addr = instruction & 0x03FFFFFF
+            print "J addr : " + hex(addr)
+            OPCODES_J[opcode](addr)
 
-		else:
-			raise TypeError('instruction "{}" : Bad opcode'.format(bin(instruction)))
+        else:
+            raise TypeError('instruction #{} ("{}") : Bad opcode'.format(self.pc / 4, bin(instruction)))
 
-	def execute_R(self, rs, rt, rd, shamt, funct):
-		"""Execute R type instruction"""
+    def execute_R(self, rs, rt, rd, shamt, funct):
+        """Execute R type instruction"""
 
-		R_DIC = {
-		 0x24 : lambda rs_reg, rt_reg, shamt: rs_reg & rt_reg, # AND
-		 0x25 : lambda rs_reg, rt_reg, shamt: rs_reg | rt_reg, # OR
-		 0x27 : lambda rs_reg, rt_reg, shamt: rs_reg ^ rt_reg, # XOR
-		 0x20 : lambda rs_reg, rt_reg, shamt: rs_reg + rt_reg, # ADD
-		 0x22 : lambda rs_reg, rt_reg, shamt: rs_reg - rt_reg, # SUB
-		 0x00 : lambda rs_reg, rt_reg, shamt: rt_reg << shamt, # SLL
-		 0x02 : lambda rs_reg, rt_reg, shamt: rt_reg >> shamt, # SRL
-		 0x2a : lambda rs_reg, rt_reg, shamt: rs_reg < rt_reg  # SLT
-		}
+        R_DIC = {
+         0x24 : lambda rs_reg, rt_reg, shamt: rs_reg & rt_reg, # AND
+         0x25 : lambda rs_reg, rt_reg, shamt: rs_reg | rt_reg, # OR
+         0x27 : lambda rs_reg, rt_reg, shamt: rs_reg ^ rt_reg, # XOR
+         0x20 : lambda rs_reg, rt_reg, shamt: rs_reg + rt_reg, # ADD
+         0x22 : lambda rs_reg, rt_reg, shamt: rs_reg - rt_reg, # SUB
+         0x00 : lambda rs_reg, rt_reg, shamt: rt_reg << shamt, # SLL
+         0x02 : lambda rs_reg, rt_reg, shamt: rt_reg >> shamt, # SRL
+         0x2a : lambda rs_reg, rt_reg, shamt: rs_reg < rt_reg  # SLT
+        }
 
-		# Execute the instruction and make sure no overflow occured
-		print "funct : {}, rs : {}, rt : {}, shamt : {}".format(funct, rs, rt, shamt)
-		res = R_DIC[funct](self.r[rs], self.r[rt], shamt)
-		res %= 0x100000000
-		# Store in output register
-		self.r[rd] = res
+        # Execute the instruction and make sure no overflow occured
+        if funct not in R_DIC:
+            raise LoaderError("Instruction #{} : bad funct ({}) in R instruction".format(self.pc / 4, hex(funct)))
+        res = R_DIC[funct](self.r[rs], self.r[rt], shamt)
+        res %= 0x100000000
+        # Store in output register
+        self.r[rd] = res
 
-	def execute_I_BEQ(self, rs, rt, immed):
-		if rs == rt:
-			self.pc += 4 * immed
+    def execute_I_BEQ(self, rs, rt, immed):
+        print "Branch ! " + str(rs == rt)
+        if immed & 1 << 15 == 1:
+            branch_addr = 0b1111 << 28
+        else:
+            branch_addr = 0x00000000
+        branch_addr |= (immed << 2)
+        if rs == rt:
+            self.pc += branch_addr
 
-	def execute_I_LW(self, rs, rt, immed):
-		self.r[rt] = self.memory.getWord[self.r[rs] + immed]
+    def execute_I_LW(self, rs, rt, immed):
+        self.r[rt] = self.memory[self.r[rs] + immed]
 
-	def execute_I_SW(self, rs, rt, immed):
-		self.memory.setWord(self.r[rt], self.r[rs] + immed)
+    def execute_I_SW(self, rs, rt, immed):
+        self.memory[self.r[rs] + immed] = self.r[rt]
 
-	def execute_I_ANDI(self, rs, rt, immed):
-		self.r[rt] = self.r[rs] & immed
+    def execute_I_ANDI(self, rs, rt, immed):
+        self.r[rt] = self.r[rs] & immed
 
-	def execute_I_ORI(self, rs, rt, immed):
-		self.r[rt] = self.r[rs] | immed
+    def execute_I_ORI(self, rs, rt, immed):
+        self.r[rt] = self.r[rs] | immed
 
-	def execute_I_ADDI(self, rs, rt, immed):
-		self.r[rt] = self.r[rs] + immed
+    def execute_I_ADDI(self, rs, rt, immed):
+        self.r[rt] = self.r[rs] + immed
 
-	def execute_J_JUMP(self, addr):
-		self.pc += addr * 4
+    def execute_J_JUMP(self, addr):
+        print "Jump ! " + hex(addr)
+        self.pc = (self.pc & (0xF << 28)) + addr * 4
 
 
 if __name__ == '__main__':
-	cpu = Cpu()
-	cpu.program.load("../gopiler/a.out")
-	for i in xrange(200):
-		cpu.step()
+    cpu = Cpu()
+    cpu.program.load("../gopiler/a.out")
+    while True:
+        cpu.step()
--- cpu_test.py
+++ cpu_test.py
@@ -1,79 +1,89 @@
 import unittest
-from cpu import *
+from cpu import Cpu
 
 class Test_execute(unittest.TestCase):
 
-	def testGeneric(self):
-		good_r = [ 0x00000000 for i in xrange(32) ]
-		cpu = Cpu()
+    def testGeneric(self):
+        good_r = [ 0x00000000 for _ in xrange(32) ]
+        cpu = Cpu()
 
-		good_r[1] = 1
-		cpu.r[1] = 1
-#		cpu.execute(0x430824)
-		self.assertEqual(cpu.r, good_r, 'AND $1, $1, $0 failed')
+        cpu.r[1] = 1
+        cpu.execute(0b00000000001000000000100000100100)
+        self.assertEqual(cpu.r, good_r, 'AND $1, $1, $0 failed')
 
+        cpu.r[1] = 1
+        good_r[1] = 1
+        cpu.execute(0b00000000001000000000100000100101)
+        self.assertEqual(cpu.r, good_r, 'OR $1, $1, $0 failed')
+
+        cpu.r[1] = 42
+        cpu.r[6] = 42 | 111
+        good_r[1] = 42
+        good_r[6] = 42 | 111
+        good_r[3] = 42
+        cpu.execute(0b00000000001001100001100000100100)
+        self.assertEqual(cpu.r, good_r, 'AND $3, $1, $6 failed')
 
 class Test_execute_R(unittest.TestCase):
 
-	def testAND(self):
-		good_r = [ 0x00000000 for i in xrange(32) ]
-		cpu = Cpu()
+    def testAND(self):
+        good_r = [ 0x00000000 for _ in xrange(32) ]
+        cpu = Cpu()
 
-		cpu.r[1] = 11
-		cpu.r[2] = 10
-		good_r[1] = 11
-		good_r[2] = 10
-		good_r[3] = 10
-		cpu.execute_R(1, 2, 3, 0, 0x24)
-		self.assertEqual(cpu.r, good_r, 'AND $1, $2, $3 failed')
+        cpu.r[1] = 11
+        cpu.r[2] = 10
+        good_r[1] = 11
+        good_r[2] = 10
+        good_r[3] = 10
+        cpu.execute_R(1, 2, 3, 0, 0x24)
+        self.assertEqual(cpu.r, good_r, 'AND $1, $2, $3 failed')
 
-		good_r[3] = 0
-		cpu.execute_R(0, 2, 3, 0, 0x24)
-		self.assertEqual(cpu.r, good_r, 'AND $0, $2, $3 failed')
+        good_r[3] = 0
+        cpu.execute_R(0, 2, 3, 0, 0x24)
+        self.assertEqual(cpu.r, good_r, 'AND $0, $2, $3 failed')
 
-	def testOR(self):
-		good_r = [ 0x00000000 for i in xrange(32) ]
-		cpu = Cpu()
+    def testOR(self):
+        good_r = [ 0x00000000 for _ in xrange(32) ]
+        cpu = Cpu()
 
-		cpu.r[1] = 11
-		cpu.r[2] = 10
-		good_r[1] = 11
-		good_r[2] = 10
-		good_r[3] = 11
-		cpu.execute_R(1, 2, 3, 0, 0x25)
-		self.assertEqual(cpu.r, good_r, 'OR $1, $2, $3 failed')
+        cpu.r[1] = 11
+        cpu.r[2] = 10
+        good_r[1] = 11
+        good_r[2] = 10
+        good_r[3] = 11
+        cpu.execute_R(1, 2, 3, 0, 0x25)
+        self.assertEqual(cpu.r, good_r, 'OR $1, $2, $3 failed')
 
-		cpu.r[31] = 0xFFFFFFFF
-		good_r[31] = 0xFFFFFFFF
-		cpu.execute_R(31, 31, 31, 0, 0x25)
-		self.assertEqual(cpu.r, good_r, 'OR $31, $31, $31 failed')
+        cpu.r[31] = 0xFFFFFFFF
+        good_r[31] = 0xFFFFFFFF
+        cpu.execute_R(31, 31, 31, 0, 0x25)
+        self.assertEqual(cpu.r, good_r, 'OR $31, $31, $31 failed')
 
-	def testSLL(self):
-		good_r = [ 0x00000000 for i in xrange(32) ]
-		cpu = Cpu()
+    def testSLL(self):
+        good_r = [ 0x00000000 for _ in xrange(32) ]
+        cpu = Cpu()
 
-		cpu.r[1] = 0x00000001
-		good_r[1] = 0x1 << 16
-		cpu.execute_R(0, 1, 1, 16, 0x00)
-		self.assertEqual(cpu.r, good_r, 'SLL $1, $1, 16 failed')
+        cpu.r[1] = 0x00000001
+        good_r[1] = 0x1 << 16
+        cpu.execute_R(0, 1, 1, 16, 0x00)
+        self.assertEqual(cpu.r, good_r, 'SLL $1, $1, 16 failed')
 
-		good_r[3] = 0
-		cpu.execute_R(1, 3, 0, 32, 0x24)
-		self.assertEqual(cpu.r, good_r, 'SLL $1, $3, 32 failed')
+        good_r[3] = 0
+        cpu.execute_R(1, 3, 0, 32, 0x24)
+        self.assertEqual(cpu.r, good_r, 'SLL $1, $3, 32 failed')
 
-	def testSLR(self):
-		good_r = [ 0x00000000 for i in xrange(32) ]
-		cpu = Cpu()
+    def testSLR(self):
+        good_r = [ 0x00000000 for _ in xrange(32) ]
+        cpu = Cpu()
 
-		cpu.r[1] = 0x80000000
-		good_r[1] = 0x1 << 15
-		cpu.execute_R(0, 1, 1, 16, 0x02)
-		self.assertEqual(cpu.r, good_r, 'SLR $1, $1, 16 failed')
+        cpu.r[1] = 0x80000000
+        good_r[1] = 0x1 << 15
+        cpu.execute_R(0, 1, 1, 16, 0x02)
+        self.assertEqual(cpu.r, good_r, 'SLR $1, $1, 16 failed')
 
-		good_r[3] = 0
-		cpu.execute_R(1, 3, 0, 32, 0x24)
-		self.assertEqual(cpu.r, good_r, 'SLR $1, $3, 32 failed')
-
+        good_r[3] = 0
+        cpu.execute_R(1, 3, 0, 32, 0x24)
+        self.assertEqual(cpu.r, good_r, 'SLR $1, $3, 32 failed')
 
 
 
