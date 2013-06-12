9a6c7b3ec021ed5d0e036edab30610c9a68a803e
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2013-06-12 18:04:30 +0200

    Create the CPU class

---
+++ cpu_test.py
@@ -0,0 +1,81 @@
+import unittest
+from cpu import *
+
+class Test_execute(unittest.TestCase):
+
+	def testGeneric(self):
+		good_r = [ 0x00000000 for i in xrange(32) ]
+		cpu = Cpu()
+
+		good_r[1] = 1
+		cpu.r[1] = 1
+#		cpu.execute(0x430824)
+		self.assertEqual(cpu.r, good_r, 'AND $1, $1, $0 failed')
+
+
+class Test_execute_R(unittest.TestCase):
+
+	def testAND(self):
+		good_r = [ 0x00000000 for i in xrange(32) ]
+		cpu = Cpu()
+
+		cpu.r[1] = 11
+		cpu.r[2] = 10
+		good_r[1] = 11
+		good_r[2] = 10
+		good_r[3] = 10
+		cpu.execute_R(1, 2, 3, 0, 0x24)
+		self.assertEqual(cpu.r, good_r, 'AND $1, $2, $3 failed')
+
+		good_r[3] = 0
+		cpu.execute_R(0, 2, 3, 0, 0x24)
+		self.assertEqual(cpu.r, good_r, 'AND $0, $2, $3 failed')
+
+	def testOR(self):
+		good_r = [ 0x00000000 for i in xrange(32) ]
+		cpu = Cpu()
+
+		cpu.r[1] = 11
+		cpu.r[2] = 10
+		good_r[1] = 11
+		good_r[2] = 10
+		good_r[3] = 11
+		cpu.execute_R(1, 2, 3, 0, 0x25)
+		self.assertEqual(cpu.r, good_r, 'OR $1, $2, $3 failed')
+
+		cpu.r[31] = 0xFFFFFFFF
+		good_r[31] = 0xFFFFFFFF
+		cpu.execute_R(31, 31, 31, 0, 0x25)
+		self.assertEqual(cpu.r, good_r, 'OR $31, $31, $31 failed')
+
+	def testSLL(self):
+		good_r = [ 0x00000000 for i in xrange(32) ]
+		cpu = Cpu()
+
+		cpu.r[1] = 0x00000001
+		good_r[1] = 0x1 << 16
+		cpu.execute_R(0, 1, 1, 16, 0x00)
+		self.assertEqual(cpu.r, good_r, 'SLL $1, $1, 16 failed')
+
+		good_r[3] = 0
+		cpu.execute_R(1, 3, 0, 32, 0x24)
+		self.assertEqual(cpu.r, good_r, 'SLL $1, $3, 32 failed')
+
+	def testSLR(self):
+		good_r = [ 0x00000000 for i in xrange(32) ]
+		cpu = Cpu()
+
+		cpu.r[1] = 0x80000000
+		good_r[1] = 0x1 << 15
+		cpu.execute_R(0, 1, 1, 16, 0x02)
+		self.assertEqual(cpu.r, good_r, 'SLR $1, $1, 16 failed')
+
+		good_r[3] = 0
+		cpu.execute_R(1, 3, 0, 32, 0x24)
+		self.assertEqual(cpu.r, good_r, 'SLR $1, $3, 32 failed')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ main.py
@@ -0,0 +1,8 @@
+import cpu
+
+def main():
+	l = Loader()
+	l.load("../gopiler/a.out")
+
+if __name__ == '__main__':
+	main()
